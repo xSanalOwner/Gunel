d2c2a51c0202717dce984c3024f1afe3d34dc935
xSanalOwner <legendmuzkao99@gmail.com>
2025-03-08 23:38:52 +0400

    Update config.py

--- config.py
+++ config.py
@@ -91,21 +91,21 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/8wngcz.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/742235264ba8c786e5954.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://files.catbox.moe/8wngcz.jpg"
+    "PING_IMG_URL", "https://telegra.ph/file/742235264ba8c786e5954.jpg"
 )
-PLAYLIST_IMG_URL = "https://files.catbox.moe/8wngcz.jpg"
-STATS_IMG_URL = "https://files.catbox.moe/8wngcz.jpg"
-TELEGRAM_AUDIO_URL = "https://files.catbox.moe/8wngcz.jpg"
-TELEGRAM_VIDEO_URL = "https://files.catbox.moe/8wngcz.jpg"
-STREAM_IMG_URL = "https://files.catbox.moe/8wngcz.jpg"
-SOUNCLOUD_IMG_URL = "https://files.catbox.moe/8wngcz.jpg"
-YOUTUBE_IMG_URL = "https://files.catbox.moe/8wngcz.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/8wngcz.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/8wngcz.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/8wngcz.jpg"
+PLAYLIST_IMG_URL = "https://telegra.ph/file/742235264ba8c786e5954.jpg"
+STATS_IMG_URL = "https://telegra.ph/file/742235264ba8c786e5954.jpg"
+TELEGRAM_AUDIO_URL = "https://telegra.ph/file/742235264ba8c786e5954.jpg"
+TELEGRAM_VIDEO_URL = "https://telegra.ph/file/742235264ba8c786e5954.jpg"
+STREAM_IMG_URL = "https://telegra.ph/file/742235264ba8c786e5954.jpg"
+SOUNCLOUD_IMG_URL = "https://telegra.ph/file/742235264ba8c786e5954.jpg"
+YOUTUBE_IMG_URL = "https://telegra.ph/file/742235264ba8c786e5954.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/742235264ba8c786e5954.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/742235264ba8c786e5954.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/742235264ba8c786e5954.jpg"
 
 
 def time_to_seconds(time):
